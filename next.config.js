2e87c640b0cf446b02edbfbaed315171c1c5ffce
Votre Nom <mika.lopez.06100@gmail.com>
2026-02-05 22:47:51 +0100

    Fix: Add webpack alias configuration for path resolution

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   // Configuration Next.js pour Vercel
-  transpilePackages: [],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
